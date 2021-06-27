1968e4fdd806bec281ab5a50876e35b2230675ed
Timothy Kipkorir Serem <kipkorirserem3@gmail.com>
2021-06-27 14:13:36 +0300

    Initial Commit

---
+++ war-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Timothy Serem on 27/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Timothy Serem on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
